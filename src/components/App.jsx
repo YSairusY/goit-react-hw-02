16b921a54684aa0ddedff951e40ecfc2c4a63bde
Artur <arturmatjo@gmail.com>
2024-11-04 14:22:51 +0100

    complete all tasks on hw

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Description from './Description/Description';
+import Feedback from './Feedback/Feedback';
+import Options from './Options/Options';
+import Notification from './Notification/Notification';
+
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem('saved-feedback');
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+  }, [feedback]);
+  const updateFeedback = feedbackType => {
+    setFeedback(prevState => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  return (
+    <div>
+      <Description />
+      <Options
+        onGood={() => updateFeedback('good')}
+        onNeutral={() => updateFeedback('neutral')}
+        onBad={() => updateFeedback('bad')}
+        onReset={() => setFeedback({ good: 0, neutral: 0, bad: 0 })}
+        totalFeedback={totalFeedback}
+        positiveFeedback={positiveFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+export default App;
